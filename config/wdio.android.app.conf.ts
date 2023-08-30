58e67300438c5efbeed6ae7115f2ed53d3ffde49
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-08-31 06:31:15 +0900

    Run android

--- config/wdio.android.app.conf.ts
+++ config/wdio.android.app.conf.ts
@@ -22,7 +22,7 @@ config.capabilities = [
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
         'appium:deviceName': 'Pixel_3_10.0',
-        'appium:platformVersion': '10.0',
+        // 'appium:platformVersion': '10.0',
         'appium:orientation': 'PORTRAIT',
         'appium:automationName': 'UiAutomator2',
         // The path to the app
