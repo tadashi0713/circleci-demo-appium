4c899c7951aa7e50c747d812623352bb7f3ac798
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-09-14 16:54:33 +0900

    report

--- config/wdio.shared.local.appium.conf.ts
+++ config/wdio.shared.local.appium.conf.ts
@@ -16,6 +16,7 @@ config.services = (config.services ? config.services : []).concat([
                 // and to automatically download the latest version of ChromeDriver
                 relaxedSecurity: true,
                 address: "localhost",
+                usePlugins: 'appium-reporter-plugin',
                 // Write the Appium logs to a file in the root of the directory
                 log: "./appium.log",
             },
